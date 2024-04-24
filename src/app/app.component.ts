3b663e7533c9b7d183b91261888a8f6406905315
Rahul Kondi <rahulkondi24@gmail.com>
2024-04-24 16:17:18 +0530

    intial commit of whiztek website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainHeaderComponent } from "./components/main-header/main-header.component";
+import { HeaderTopComponent } from "./components/header-top/header-top.component";
+import { MainFooterComponent } from "./components/main-footer/main-footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, MainHeaderComponent, HeaderTopComponent, MainFooterComponent,CommonModule]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isLoading: boolean = true; // Initially show loader
+
+  ngOnInit() {
+    // Simulate loading data with setTimeout
+    setTimeout(() => {
+      this.isLoading = false; // Hide loader after data is loaded
+    }, 700); // Replace with actual data loading logic
+  }
+
   title = 'whiztek';
 }
---
+++ src/app/components/main-footer/main-footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-footer.component.html',
+  styleUrl: './main-footer.component.scss'
+})
+export class MainFooterComponent {
+  currentYear: number;
+
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+  }
+}
